157aba0233639afdd5dc47cc7eda48b64319fb6b
Prayag <parajuliprayag234@gmail.com>
2026-06-16 16:11:09 +0545

    routine.h updated

---
+++ models/FAQ.h
@@ -0,0 +1,26 @@
+models/FAQ.h
+
+
+#ifndef FAQ_H
+#define FAQ_H
+
+#include <QString>
+
+class Faq
+{
+private:
+    QString question;
+    QString answer;
+
+public:
+    Faq();
+    Faq(QString question, QString answer);
+
+    QString getQuestion() const;
+    QString getAnswer() const;
+
+    void setQuestion(QString question);
+    void setAnswer(QString answer);
+};
+
+#endif
