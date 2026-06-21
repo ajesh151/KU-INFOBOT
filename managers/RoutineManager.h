f109c41c77bec2ae4ba57c13528da4a77713a02c
Prayag <parajuliprayag234@gmail.com>
2026-06-21 20:36:37 +0545

    RoutineManager.h edit

--- managers/RoutineManager.h
+++ managers/RoutineManager.h
@@ -22,12 +22,13 @@ public:
 
     QList<Routine> getBySemester(int semester) const;
 
-    QList<Routine> getByProgramAndSemester(
-        const QString& program,
-        int semester) const;
+    QList<Routine> getByProgramAndSemester(onst QString& program,int semester) const;
 
-    QList<Routine> getByCourseCode(
-        const QString& courseCode) const;
+    QList<Routine> getByCourseCode( const QString& courseCode) const;
+
+    QList<Routine> search(const QString& program = QString(), int year = -1,
+                         int semester = -1,const QString& section = QString(),
+                         const QString& day = QString(),const QString& courseCode = QString()) const;
 
     int size() const;
 };
