e9629ad9746a7c8bae0154d15a324ed350c1ad74
Prayag <parajuliprayag234@gmail.com>
2026-06-25 20:28:51 +0545

    FAQ.cpp updates

--- models/FAQ.cpp
+++ models/FAQ.cpp
@@ -4,8 +4,8 @@ Faq::Faq()
 {
 }
 
-Faq::Faq(const QString question,
-         const QString answer)
+Faq::Faq(const QString &question,
+         const QString &answer)
     : question(question),
     answer(answer)
 {
@@ -21,12 +21,12 @@ QString Faq::getAnswer() const
     return answer;
 }
 
-void Faq::setQuestion( QString& question)
+void Faq::setQuestion( const QString& question)
 {
     this->question = question;
 }
 
-void Faq::setAnswer( QString& answer)
+void Faq::setAnswer( const QString& answer)
 {
     this->answer = answer;
 }
